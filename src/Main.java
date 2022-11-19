5105174f58320dd0ccdf6cf47a40d227356c9c2e
Ivan test <ivan02081983s@ya.ru>
2022-11-19 11:02:59 +0300

    gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int score = 100;
-        int deposit_amount = 900;
+        int depositAmount = 1000;
         int bonus;
         int amount;
-        if (deposit_amount >= 1000) {
-            bonus = deposit_amount / 100;
-            amount = bonus + score + deposit_amount;
+        if (depositAmount > 1000) {
+            bonus = depositAmount / 100;
+            amount = bonus + score + depositAmount;
             System.out.println("Бонус равен: " + bonus);
             System.out.println("Итоговая сумма на счету клиента: " + amount);
         } else {
             System.out.println("Бонусов нет.");
-            amount = score + deposit_amount;
+            amount = score + depositAmount;
             System.out.println("Итоговая сумма на счету клиента: " + amount);
         }
     }
